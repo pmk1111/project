a84e3b31e59f8cad3daa76c2f312c5e9f76e2465
pmk1111 <kingpmk1004@naver.com>
2023-08-11 14:21:35 +0900

    댓글 삭제 완료

--- project/src/main/java/net/comment/db/CommentDAO.java
+++ project/src/main/java/net/comment/db/CommentDAO.java
@@ -13,7 +13,7 @@ import com.google.gson.JsonObject;
 
 public class CommentDAO {
 	private DataSource ds;
-	
+
 	public CommentDAO() {
 		try {
 			Context init = new InitialContext();
@@ -26,52 +26,52 @@ public class CommentDAO {
 	public int commentsInsert(Comment co) {
 		int result = 0;
 		String sql = "insert into comm "
-				   + "(c_num, c_id, c_content, reg_date, comment_i_num, "
-				   + "comment_re_lev, comment_re_seq, comment_re_ref)"
-				   + " values(com_seq.nextval,?,?,sysdate,?,0,0,com_seq.nextval)";
-		
+				+ "(c_num, c_id, c_content, reg_date, comment_i_num, "
+				+ "comment_re_lev, comment_re_seq, comment_re_ref)"
+				+ " values(com_seq.nextval,?,?,sysdate,?,0,0,com_seq.nextval)";
+
 		try(Connection con = ds.getConnection();
 				PreparedStatement pstmt = con.prepareStatement(sql);){
-			
-//			pstmt.setInt(1, co.getNum());
+
+			//			pstmt.setInt(1, co.getNum());
 			pstmt.setString(1, co.getC_id());
 			pstmt.setString(2, co.getC_content());
 			pstmt.setInt(3, co.getComment_i_num());
-//			pstmt.setInt(4, co.getComment_p_num());
+			//			pstmt.setInt(4, co.getComment_p_num());
 			result = pstmt.executeUpdate();
 			if(result ==1) {
 				System.out.println("데이터 삽입 완료");
 			}
-			
+
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
 		return result;
 	} //commentsInsert end
-	
-	
+
+
 	public int getListCount(int comment_i_num) {
 		int x = 0;
 		String sql = "select count(*) "
-				   + "from comm "
-				   + "where comment_i_num = ? ";
+				+ "from comm "
+				+ "where comment_i_num = ? ";
 		try(Connection con = ds.getConnection();
 				PreparedStatement pstmt = con.prepareStatement(sql);){
-			
+
 			pstmt.setInt(1,comment_i_num);
-			
-		try(ResultSet rs= pstmt.executeQuery()){
-			if(rs.next()) {
-				x = rs.getInt(1);
+
+			try(ResultSet rs= pstmt.executeQuery()){
+				if(rs.next()) {
+					x = rs.getInt(1);
+				}
 			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+
 		}
-	}catch(Exception ex) {
-		ex.printStackTrace();
-		
+		return x;
 	}
-	return x;
-}
-	
+
 	public JsonArray getCommentList(int comment_i_num, int state) {
 		String sort="asc"; //등록순
 		if(state==2) {//최신순
@@ -88,14 +88,14 @@ public class CommentDAO {
 		JsonArray array = new JsonArray();
 		try(Connection con = ds.getConnection();
 				PreparedStatement pstmt = con.prepareStatement(sql);){
-			
+
 			pstmt.setInt(1,  comment_i_num);
 			try(ResultSet rs = pstmt.executeQuery()){
-				
+
 				while(rs.next()) {
 					JsonObject object = new JsonObject();
 					object.addProperty("c_num", rs.getInt(1));
-//					object.addProperty("num", rs.getInt(2));
+					//					object.addProperty("num", rs.getInt(2));
 					object.addProperty("c_id", rs.getString(2));
 					object.addProperty("c_content", rs.getString(3));
 					object.addProperty("reg_date", rs.getString(4));
@@ -113,4 +113,21 @@ public class CommentDAO {
 		return array;
 	}//getCommentList()메서드 end
 
+
+	public int commentsDelete(int c_num) {
+		int result = 0;
+
+		String sql = "delete comm where c_num = ?";
+		try (Connection con = ds.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);){
+
+			pstmt.setInt(1, c_num);
+			result = pstmt.executeUpdate();
+			if(result == 1) System.out.println("댓글 삭제 성공");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+
+	}
 }
--- project/src/main/java/net/issue/action/IssueFrontController.java
+++ project/src/main/java/net/issue/action/IssueFrontController.java
@@ -9,95 +9,99 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import net.comment.action.CommentAdd;
+import net.comment.action.CommentDeleteAction;
 import net.comment.action.CommentList;
 
 
 
 @WebServlet("*.bo")
 public class IssueFrontController extends javax.servlet.http.HttpServlet {
-   private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
 
-   protected void doProcess(HttpServletRequest request, HttpServletResponse response) 
-         throws ServletException, IOException {
-      /*
-       * 요청된 전체 URL중에서 포트 번호 다음 부터 마지막 문자열까지 반환합니다.
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		/*
+		 * 요청된 전체 URL중에서 포트 번호 다음 부터 마지막 문자열까지 반환합니다.
           예)  contextPath가 "/JspProject"인 경우
                http://localhost:8088/JspProject/login.net로 요청하면 getRequestURI()는
                "/JspProject/login.net"반환됩니다.
-       */
-      String RequestURI = request.getRequestURI();
-      System.out.println("RequestURI = " + RequestURI);
+		 */
+		String RequestURI = request.getRequestURI();
+		System.out.println("RequestURI = " + RequestURI);
 
-      //getContextPath() : 컨텍스트 경로가 반환됩니다.
-      //contextPath는 "/JspProject"가 반환됩니다.
-      String contextPath = request.getContextPath();
-      System.out.println("contextPath = " + contextPath);
+		//getContextPath() : 컨텍스트 경로가 반환됩니다.
+		//contextPath는 "/JspProject"가 반환됩니다.
+		String contextPath = request.getContextPath();
+		System.out.println("contextPath = " + contextPath);
 
-      //RequestURI에서 컨텍스트 경로 길이 값의 인덱스 위치의 문자부터 마지막 위치 문자까지 추출합니다.
-      //command는 "/login.net" 반환합니다.
-      String command = RequestURI.substring(contextPath.length());
-      System.out.println("command="+command);
+		//RequestURI에서 컨텍스트 경로 길이 값의 인덱스 위치의 문자부터 마지막 위치 문자까지 추출합니다.
+		//command는 "/login.net" 반환합니다.
+		String command = RequestURI.substring(contextPath.length());
+		System.out.println("command="+command);
 
-      //초기화
-      ActionForward forward = null;
-      Action action = null;
+		//초기화
+		ActionForward forward = null;
+		Action action = null;
 
-      switch (command) {
-      case "/IssueList.bo":
-         action = new IssueListAction();
-         break;
-      case "/IssueAddAction.bo":
-         action = new IssueAddAction();
-         break;
-      case "/IssueDetailAction.bo":
-         action = new IssueDetailAction();
-         break;
-      case "/IssueModifyAction.bo":
-         action = new IssueModifyAction();
-         break;
-   
-                  case "/IssueDeleteAction.bo":
-                     action = new IssueDeleteAction();
-                     break;   
-         
-               case "/IssueDeleteProcess.bo":
-                  action = new IssueDeleteProcess();
-                  break;  
+		switch (command) {
+		case "/IssueList.bo":
+			action = new IssueListAction();
+			break;
+		case "/IssueAddAction.bo":
+			action = new IssueAddAction();
+			break;
+		case "/IssueDetailAction.bo":
+			action = new IssueDetailAction();
+			break;
+		case "/IssueModifyAction.bo":
+			action = new IssueModifyAction();
+			break;
 
-                  case "/CommentAdd.bo":
-                     action = new CommentAdd();
-                     break;
-                     
-                  case "/CommentList.bo":
-                     action = new CommentList();
-                     break;
+		case "/IssueDeleteAction.bo":
+			action = new IssueDeleteAction();
+			break;   
 
-      }//switch end
-      forward = action.execute(request, response);
+		case "/IssueDeleteProcess.bo":
+			action = new IssueDeleteProcess();
+			break;  
 
-      if(forward !=null) {
-         if(forward.isRedirect()) {//리다이렉트 됩니다.
-            response.sendRedirect(forward.getPath());
-         }else {//포워딩됩니다.
-            RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
-            dispatcher.forward(request, response);
-         }
-      }
-   }//doProcess() end
+		case "/CommentAdd.bo":
+			action = new CommentAdd();
+			break;
 
-   //doProcess(request,response)메서드를 구현하여 요청이 GET방시이든
-   //POST방식으로 전송되어 오든 같은 메서드에서 요청을 처리할 수 있도록 하였습니다.
-   protected void doGet(HttpServletRequest request, HttpServletResponse response)
-         throws ServletException, IOException {
-      doProcess(request, response);
-   }
+		case "/CommentList.bo":
+			action = new CommentList();
+			break;
+		case "/CommentDelete.bo": 
+			action = new CommentDeleteAction();
+			break;
 
-   protected void doPost(HttpServletRequest request, HttpServletResponse response)
-         throws ServletException, IOException {
-      request.setCharacterEncoding("utf-8");
-      doProcess(request, response);
-   }
+		}//switch end
+		forward = action.execute(request, response);
+
+		if(forward !=null) {
+			if(forward.isRedirect()) {//리다이렉트 됩니다.
+				response.sendRedirect(forward.getPath());
+			}else {//포워딩됩니다.
+				RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
+				dispatcher.forward(request, response);
+			}
+		}
+	}//doProcess() end
+
+	//doProcess(request,response)메서드를 구현하여 요청이 GET방시이든
+	//POST방식으로 전송되어 오든 같은 메서드에서 요청을 처리할 수 있도록 하였습니다.
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		doProcess(request, response);
+	}
 
 
 
